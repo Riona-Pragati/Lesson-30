8d98fb24846444ef2264bee48a67091114acf3ad
Riona-Pragati <renuprasanna2012@gmail.com>
2026-01-21 20:11:34 -0500

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,21 @@
+my_set = {1,2,2,3,4,4,4}
+print("Set: ", my_set)
+
+my_set.add(5)
+print("Updated Set: ", my_set)
+
+set1 = my_set
+set2 = {2,4,4,6}
+
+print("\nSet 1", set1)
+print("set2", set2)
+print("Difference")
+print(set1.difference(set2))
+print("Symmetric Difference")
+print(set1.symmetric_difference(set2))
+
+setc1 = {"green", "blue"}
+setc2 = {"blue", "Yellow"}
+print("\nUnion of above sets: ")
+print(setc1.union(setc2))
+print(setc1.intersection(setc2))
